e4c6e64e8029a97aad7a98a8924a845ec5005b91
LuWroblewski <wrolucas@gmail.com>
2024-12-02 19:59:20 -0300

    add menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideAlignJustify, lucideGithub, lucideLinkedin } from '@ng-icons/lucide';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { WorksComponent } from '../works/works.component';
 
 @Component({
   selector: 'app-menu',
-  imports: [NgIcon],
+  imports: [NgIcon, HomeComponent, AboutComponent, WorksComponent],
   viewProviders: [provideIcons({ lucideAlignJustify, lucideGithub, lucideLinkedin })],
   templateUrl: './menu.component.html',
 })
