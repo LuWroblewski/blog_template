63002d4808e1510bacc5dc235ea1f8a98a7d440a
LuWroblewski <wrolucas@gmail.com>
2024-12-03 17:47:49 -0300

    add icons, footer and links

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideAlignJustify, lucideGithub, lucideLinkedin } from '@ng-icons/lucide';
+
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { WorksComponent } from '../works/works.component';
+import { ContactComponent } from '../contact/contact.component';
+
+import { lucideAlignJustify, lucideLinkedin } from '@ng-icons/lucide';
+import { tdesignLogoBehance } from '@ng-icons/tdesign-icons';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-menu',
-  imports: [NgIcon, HomeComponent, AboutComponent, WorksComponent],
-  viewProviders: [provideIcons({ lucideAlignJustify, lucideGithub, lucideLinkedin })],
+  imports: [NgIcon, HomeComponent, AboutComponent, WorksComponent, ContactComponent, FooterComponent],
+  viewProviders: [provideIcons({ lucideAlignJustify, lucideLinkedin, tdesignLogoBehance })],
   templateUrl: './menu.component.html',
 })
 export class MenuComponent {}
