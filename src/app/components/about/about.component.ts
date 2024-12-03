ebf3835a0380b7792deb0526989858276ef6d964
LuWroblewski <wrolucas@gmail.com>
2024-12-03 17:46:57 -0300

    add html and informations

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideStar } from '@ng-icons/lucide';
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [NgIcon],
+  viewProviders: [provideIcons({ lucideStar })],
   templateUrl: './about.component.html',
 })
 export class AboutComponent {}
